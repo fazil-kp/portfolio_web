6195cfd7757b84147aad1c8e8b2f6268dc8fd305
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-07-18 20:58:25 +0530

    Updating

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "990487543"
+    serviceWorkerVersion: "935675614"
   }
 });
