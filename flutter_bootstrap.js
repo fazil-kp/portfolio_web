02867040fcbcd0e9248e7e7e16926972673ac153
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-07-21 21:04:54 +0530

    portfoilio updating july 21

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2464854013"
+    serviceWorkerVersion: "3786778968"
   }
 });
